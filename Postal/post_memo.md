9e3cc49a46ba86e0df72b108d8c61ba1184931d7
ninnin92 <ninnin92.28@gmail.com>
2015-07-19 18:04:43 +0900

    郵便番号から住所データの整理：Windows対応

--- Postal/post_memo.md
+++ Postal/post_memo.md
@@ -1,15 +1,39 @@
+## 郵便番号による住所検索、参加者DBの更新
+
+### 郵便番号のDBを取得
+#### メモ
+* 日本郵便HPからCSVで郵便番号リストをダウンロードできる
+  * [郵便番号データダウンロード](http://www.post.japanpost.jp/zipcode/dl/kogaki-zip.html)
+  * 12万行もあるのでとてもじゃないけどCSVでは扱えない
+  * 1ヶ月に一度くらい更新されてるっぽい？
+* CSVをsqlのデータベースに移す、DBの更新ができると良い（その都度作りなおしてもいいけど）  
+
+＜参考になりそうなサイト＞
+* 郵便番号csv → sqlite  
+ * [PythonでSQLiteを使ってみる](http://bty.sakura.ne.jp/wp/archives/333)
+ * [MySQLへ郵便番号データをインポートする](http://plaza.rakuten.co.jp/pgmemo/diary/200512110000/)
+ * [プログラミング講座：SQLite を使って郵便番号から住所を検索するプログラム](http://www.mysticwall.com/course/postal01.html)
+ * [郵便番号データについて思ったこと | Studio JamPack](http://jamfunk.jp/wp/?p=390)
+
+* pyhonでsqlite
+ * [Pythonのsqlite3ライブラリでデータベースを操作しよう](http://msrx9.bitbucket.org/blog/html/2013/07/04/db_study.html)
+ * [SQLite](http://www.python-izm.com/contents/external/sqlite.shtml)
+ * [データベース（sqlite）](https://www.quark.kj.yamagata-u.ac.jp/~hiroki/python/?id=16)  
+
+
 * 日本郵便の郵便番号CSVはいろいろとヒドイ（郵便番号データの落とし穴）
- * 加工済みデータが配布されているらしいのでそれを使うことにする
- * [郵便番号データのダウンロード - zipcloud](http://zipcloud.ibsnet.co.jp/)
- * 更新にも対応してくれるらしいので、ときたま更新すべきかも（半年とか年一とか）
+  * 加工済みデータが配布されているらしいのでそれを使うことにする
+  * [郵便番号データのダウンロード - zipcloud](http://zipcloud.ibsnet.co.jp/)
+  * 更新にも対応してくれるらしいので、ときたま更新すべきかも（半年とか年一とか)  
+
 
 * 郵便データの落とし穴
- *[郵便番号データの落とし穴 – YU-TANG's MS-Access Discovery](http://www.f3.dion.ne.jp/~element/msaccess/AcTipsKenAllCsv.html)
- *[郵便番号データについて思ったこと|Studio Jampack](http://jamfunk.jp/wp/?p=390)
- *[郵便番号から住所を検索するサービスにまともなものがない – ぐるぐる～](http://d.hatena.ne.jp/bleis-tift/20080531/1212217681)
+  * [郵便番号データの落とし穴 – YU-TANG's MS-Access Discovery](http://www.f3.dion.ne.jp/~element/msaccess/AcTipsKenAllCsv.html)
+  * [郵便番号データについて思ったこと|Studio Jampack](http://jamfunk.jp/wp/?p=390)
+  * [郵便番号から住所を検索するサービスにまともなものがない – ぐるぐる～](http://d.hatena.ne.jp/bleis-tift/20080531/1212217681)
 
  * エラー処理
-  *　郵便番号
-   * 7桁かどうか
-   * 数値のみで構成されているか
-   * ハイフンは入っているか？（入っていれば削除）
+   * 郵便番号
+     * 7桁かどうか
+     * 数値のみで構成されているか
+     * ハイフンは入っているか？（入っていれば削除）
--- Postal/postal.py
+++ Postal/postal.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env
 # -*- coding: utf-8 -*-
 
+
 #######################################################
 # pythonからデータベースにアクセス
 #######################################################
@@ -10,6 +11,7 @@ import pandas as pd
 import re
 import sqlite3 as sq
 import unicodedata as ud
+import sys
 
 #######################################################
 # いろいろと準備
@@ -24,6 +26,7 @@ fp = open("output/Error_" + today + ".txt", "w")
 # 郵便番号DBの読み込み
 post_db = sq.connect("postal.db")
 cur = post_db.cursor()
+
 
 # 参加者DBの読み込み
 df = pd.read_csv("参加者リスト.csv", index_col="研究者番号", encoding="utf-8")
@@ -111,76 +114,84 @@ def number_address(ID, right, pref, city, addr):
 #######################################################
 # メイン処理
 #######################################################
-
 if __name__ == '__main__':
-    print("#Run process")
-    # 参加者DBから一行づつ住所列を作成
-    for ID, info in postcodes.iterrows():  # 第二の返り値はSereis型で返ってくるので下で分割
-        code = info["郵便番号"]
-        right = info["住所"]
+    try:
+        print("#Run process")
+        # 参加者DBから一行づつ住所列を作成
+        for ID, info in postcodes.iterrows():  # 第二の返り値はSereis型で返ってくるので下で分割
+            code = info["郵便番号"]
+            right = info["住所"]
 
-        pcode = arrange_postcode(code)  # 郵便番号の形式を整える
-        print("Postcode:  " + pcode)
+            pcode = arrange_postcode(code)  # 郵便番号の形式を整える
+            print("Postcode:  " + pcode)
 
-        if len(pcode) == 7 and pcode.isdigit():  # 7桁の数字であるもののみ処理
-            try:
-                pref, city, addr = post_address(pcode)  # 郵便番号DBから住所を取得
-            except:
-                Error_post.append(ID)  # よく分かんないエラーをエラーリストに放り込む
+            if len(pcode) == 7 and pcode.isdigit():  # 7桁の数字であるもののみ処理
+                try:
+                    pref, city, addr = post_address(pcode)  # 郵便番号DBから住所を取得
+                except:
+                    Error_post.append(ID)  # よく分かんないエラーをエラーリストに放り込む
+                    pref, city, addr = "NaN", "NaN", "NaN"  # とりあえず0で埋める
+            else:
+                Error_post.append(ID)  # 郵便番号でないものをエラーリストに放り込む
                 pref, city, addr = "NaN", "NaN", "NaN"  # とりあえず0で埋める
-        else:
-            Error_post.append(ID)  # 郵便番号でないものをエラーリストに放り込む
-            pref, city, addr = "NaN", "NaN", "NaN"  # とりあえず0で埋める
 
-        # 元の住所から、郵便番号から取得した住所を使った置き換えで番地等を抽出
-        try:
-            number = number_address(ID, right, pref, city, addr)
-        except:
-            number = "NaN"
+            # 元の住所から、郵便番号から取得した住所を使った置き換えで番地等を抽出
+            try:
+                number = number_address(ID, right, pref, city, addr)
+            except:
+                number = "NaN"
 
-        # データから１行作成
-        df_addr = pd.DataFrame({"研究者番号": [ID],
-                                "郵便番号": [pcode],
-                                "都道府県": [pref],
-                                "市町村": [city],
-                                "町域": [addr],
-                                "番地等": [number]},
-                               columns=["研究者番号", "郵便番号", "都道府県",
-                                        "市町村", "町域", "番地等"])
+            # データから１行作成
+            df_addr = pd.DataFrame({"研究者番号": [ID],
+                                    "郵便番号": [pcode],
+                                    "都道府県": [pref],
+                                    "市町村": [city],
+                                    "町域": [addr],
+                                    "番地等": [number]},
+                                   columns=["研究者番号", "郵便番号", "都道府県",
+                                            "市町村", "町域", "番地等"])
 
-        # 住所列に１行づつ追加していく
-        df_write = pd.concat([df_write, df_addr])
+            # 住所列に１行づつ追加していく
+            df_write = pd.concat([df_write, df_addr])
 
-    # 住所列のindexを再設定
-    df_write = df_write.set_index(["研究者番号"])
+        # 住所列のindexを再設定
+        df_write = df_write.set_index(["研究者番号"])
 
-    # 元のデータベースを使って、新しく住所列を追加
-    df = df.drop(["郵便番号"], axis=1)  # 整えた後の郵便番号に統一するために、いちど郵便番号列を削除
-    df = pd.concat([df, df_write], axis=1)  # 元のデータフレーム + 住所列
-    df.to_csv(output_path)  # 新規でCSV出力
-    print("#新_参加者DB")
-    print(df)
+        # 元のデータベースを使って、新しく住所列を追加
+        df = df.drop(["郵便番号"], axis=1)  # 整えた後の郵便番号に統一するために、いちど郵便番号列を削除
+        df = pd.concat([df, df_write], axis=1)  # 元のデータフレーム + 住所列
+        df.to_csv(output_path)  # 新規でCSV出力
+        print("#新_参加者DB")
+        print(df)
 
-    # エラーが出たものを別フォルダにアウトプット
-    print("#Error_ID")
-    print("##Error_post")
-    print(Error_post)
-    print("##Error_address")
-    print(Error_address)
+        # エラーが出たものを別フォルダにアウトプット
+        print("#Error_ID")
+        print("##Error_post")
+        print(Error_post)
+        print("##Error_address")
+        print(Error_address)
 
-    # 郵便番号→住所でエラーがでたもの
-    if len(Error_post) > 0:
-        fp.write("##Error_post" + "\n")
-        for m in Error_post:
-            fp.write(str(m) + "\n")
+        # 郵便番号→住所でエラーがでたもの
+        if len(Error_post) > 0:
+            fp.write("##Error_post" + "\n")
+            for m in Error_post:
+                fp.write(str(m) + "\n")
 
-    # 住所→番地等でエラーがでたもの
-    if len(Error_address) > 0:
-        fp.write("##Error_address" + "\n")
-        for n in Error_address:
-            fp.write(str(n) + "\n")
+        # 住所→番地等でエラーがでたもの
+        if len(Error_address) > 0:
+            fp.write("##Error_address" + "\n")
+            for n in Error_address:
+                fp.write(str(n) + "\n")
 
-    print("#End process")
+        print("#End process")
+
+    except Exception as e:
+        print('=== エラー内容 ===')
+        print('type:' + str(type(e)))
+        print('args:' + str(e.args))
+        print('message:' + e.message)
+        print('e自身:' + str(e))
+        input()
 
 # いろいろ終了
 fp.close()  # エラーリストを閉じる
